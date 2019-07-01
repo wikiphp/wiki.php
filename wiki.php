6cf8c6068184e180a2a693a1bded319af19e9e15
Ubergeek <ubergeek@yourtilde.com>
2019-07-01 15:05:01 +0000

    Cleaned up indents, added commenting for blocks

--- wiki.php
+++ wiki.php
@@ -14,27 +14,45 @@ print "<!DOCTYPE html>
 		<title>$site_name - $page</title>
 		<link rel='stylesheet' type='text/css' href='$site_root/includes/site.css'>
 	</head>
-	<body>";
-echo ( shell_exec("/usr/bin/pandoc $doc_root/includes/header.md") );
+	<body>
+<!-- Begin Header -->
+";
 
-print "<hr/>
+echo ( shell_exec("/usr/bin/pandoc $doc_root/includes/header.md") );
+print "<!-- End Header -->
+";
+
+print "<hr>
 	<div id='body' style='width: 90%;'>
-		<div id='sidebar' style='width: 20%;float:left;'>";
+
+<!-- Begin Sidebar  -->
+		<div id='sidebar' style='width: 20%;float:left;'>
+";
 echo ( shell_exec("/usr/bin/pandoc $doc_root/includes/sidebar.md") );
 
-print "	</div>
-	<div id='content' style='width: 80%; float:right;'>";
+print "		</div>
+<!-- End Sidebar -->
+
+<!-- Begin Body -->
+		<div id='content' style='width: 80%; float:right;'>";
 
 echo ( shell_exec("/usr/bin/pandoc $doc_root/articles/$page.md") );
 
-print "	</div>
+print "		</div>
+<!-- End Body -->
+
 	</div>
+
+<!-- Begin Footer -->
 	<div id='footer' style='clear:both;'>
-	<hr/>";
+	<hr>
+";
 
 echo ( shell_exec("/usr/bin/pandoc $doc_root/includes/footer.md") );
 
-print "		</div>
+print "	</div>
+<!-- End Footer -->
+
 	</body>
 </html>";
 ?>
